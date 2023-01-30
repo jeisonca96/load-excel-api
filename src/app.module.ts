1f4837624b602349b9ab0801a02596ece6f928c1
Jeison Aparicio <jeisonca96@hotmail.com>
2023-01-29 22:51:19 -0500

    feat: added config and mongoose

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,19 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { MongooseModule } from '@nestjs/mongoose';
+import { EventEmitterModule } from '@nestjs/event-emitter';
+import { AppConfig } from './config/app.config';
 import { HealthModule } from './health/health.module';
 import { LoadExcelFilesModule } from './load-excel-files/load-excel-files.module';
 
 @Module({
-  imports: [HealthModule, LoadExcelFilesModule],
-  providers: [],
+  imports: [
+    ConfigModule.forRoot(),
+    MongooseModule.forRoot(process.env.DATABASES_MONGO_URL),
+    EventEmitterModule.forRoot(),
+    HealthModule,
+    LoadExcelFilesModule,
+  ],
+  providers: [AppConfig],
 })
 export class AppModule {}
