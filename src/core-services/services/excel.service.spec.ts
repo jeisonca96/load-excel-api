4a490c7a9386a7b08b36f563b587dc37149413a3
Jeison Aparicio <jeisonca96@hotmail.com>
2023-01-30 02:53:29 -0500

    feat: added unit test for excel service

--- src/core-services/services/excel.service.spec.ts
+++ src/core-services/services/excel.service.spec.ts
@@ -21,10 +21,38 @@ describe('ExcelService', () => {
 
       const data = await service.readExcel(buffer);
 
-      // expect(data).toEqual([
-      //   { name: 'Esteban', age: 30 },
-      //   { name: 'Maria', age: 25 },
-      // ]);
+      expect(data).toEqual({
+        data: [
+          { name: 'Esteban', age: 30 },
+          { name: 'Maria', age: 25 },
+        ],
+        processTime: expect.any(Number),
+      });
+    });
+
+    it('should return an array of errors', async () => {
+      const sheet = XLSX.utils.aoa_to_sheet([
+        ['name', 'age'],
+        ['Esteban', 30],
+        ['Maria', 'any string'],
+      ]);
+      const workbook = XLSX.utils.book_new();
+      XLSX.utils.book_append_sheet(workbook, sheet, 'Sheet1');
+      const buffer = XLSX.write(workbook, { type: 'buffer', bookType: 'xlsx' });
+
+      const data = await service.readExcel(buffer);
+
+      expect(data).toEqual({
+        errors: [
+          {
+            message: 'Property age must be a number',
+            value: 'any string',
+            row: 3,
+            column: 'age',
+          },
+        ],
+        processTime: expect.any(Number),
+      });
     });
   });
 });
